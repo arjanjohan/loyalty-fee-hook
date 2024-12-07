a1e4fc5bfff523c279dbc9a221055e22c1c90f74
arjanjohan <arjanvanhoogdalem@gmail.com>
2024-12-07 22:47:13 +0100

    updated contract

--- src/lib.rs
+++ src/lib.rs
@@ -60,14 +60,14 @@ sol_storage! {
 
         mapping(address => mapping(address => uint256)) points;
         mapping(address => mapping(address => uint256)) last_activity_block;
-        uint24 base_fee;
+        uint256 base_fee;
         uint256 expiration_blocks;
     }
 
 
     struct Tier {
         uint256 threshold;
-        uint24 discount; // in basis points (10000 = 100%)
+        uint256 discount; // in basis points (10000 = 100%)
     }
 }
 
@@ -75,6 +75,7 @@ sol_storage! {
 ///
 /// NOTE: The contract's interface can be modified in any way.
 pub trait IFeeLogic {
+    fn init(&mut self);
 
     fn get_user_points(&self, user: Address, currency_1: Address) -> Result<U256, Error>;
 
@@ -95,6 +96,15 @@ pub trait IFeeLogic {
 /// with the following external methods.
 #[public]
 impl IFeeLogic for FeeLogic {
+    fn init(&mut self) {
+        self.base_fee.set(U256::from(50000));
+        self.expiration_blocks.set(U256::from(100));
+        for i in 0..3 {
+            let threshold = U256::from(1000000000000000000);
+            let tier = Tier { threshold: threshold, discount: U256::from(5000) };
+            let x = self.tiers.setter(i).insert(tier);
+        }
+    }
 
     fn get_user_points(&self, user: Address, currency_1: Address) -> Result<U256, Error> {
         Ok(U256::from(self.points.get(currency_1).get(user)))
@@ -119,7 +129,7 @@ impl IFeeLogic for FeeLogic {
         zero_for_one: bool,
         amount_specified: I256,
         delta_amount_0: I256,
-        currency_0: Address,
+        _currency_0: Address,
         currency_1: Address,
     ) -> Result<(), Error> {
         // Calculate points earned with swap
@@ -176,4 +186,69 @@ impl FeeLogic {
             }
         }
     }
+}
+
+
+/// Unit tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use alloy_primitives::{address, uint};
+
+    const CURRENCY_ETH: Address = address!("0000000000000000000000000000000000000000");
+    const CURRENCY_1: Address = address!("A11CEacF9aa32246d767FCCD72e02d6bCbcC375d");
+    const CURRENCY_2: Address = address!("B0B0cB49ec2e96DF5F5fFB081acaE66A2cBBc2e2");
+
+    #[test]
+    fn sample_test() {
+        assert_eq!(4, 2 + 2);
+    }
+
+    #[motsu::test]
+    fn calculates_points_calculation(contract: FeeLogic) {
+
+        let swap_amount = 100.try_into().unwrap();
+
+        // Test baseline values
+        let user = address!("199d51a2Be04C65f325908911430E6FF79a15ce3");
+        let user_points = contract.get_user_points(user, CURRENCY_1).expect("should get user points for currency 1");
+        assert_eq!(user_points, U256::ZERO);
+
+        // // Update points
+        contract.update_points(user, true, swap_amount, swap_amount, CURRENCY_ETH, CURRENCY_1).expect("should update points");
+        // Check new values, points should be equal to swap amount
+        let user_points = contract.get_user_points(user, CURRENCY_1).expect("should get user points for currency 1");
+        assert_eq!(user_points, swap_amount.unsigned_abs());
+
+    }
+
+    // #[motsu::test]
+    // fn calculates_amount_out(contract: UniswapCurve) {
+    //     let amount_in = uint!(2_U256);
+    //     let expected_amount_out = amount_in; // 1:1 swap
+    //     let amount_out = contract
+    //         .calculate_amount_out(amount_in, CURRENCY_1, CURRENCY_2, true)
+    //         .expect("should calculate `amount_out`");
+    //     assert_eq!(expected_amount_out, amount_out);
+    // }
+
+    // #[motsu::test]
+    // fn returns_amount_in_for_exact_output(contract: UniswapCurve) {
+    //     let amount_out = uint!(1_U256);
+    //     let expected_amount_in = amount_out; // 1:1 swap
+    //     let amount_in = contract
+    //         .get_amount_in_for_exact_output(amount_out, CURRENCY_1, CURRENCY_2, true)
+    //         .expect("should calculate `amount_in`");
+    //     assert_eq!(expected_amount_in, amount_in);
+    // }
+
+    // #[motsu::test]
+    // fn returns_amount_out_from_exact_input(contract: UniswapCurve) {
+    //     let amount_in = uint!(2_U256);
+    //     let expected_amount_out = amount_in; // 1:1 swap
+    //     let amount_out = contract
+    //         .get_amount_out_from_exact_input(amount_in, CURRENCY_1, CURRENCY_2, true)
+    //         .expect("should calculate `amount_out`");
+    //     assert_eq!(expected_amount_out, amount_out);
+    // }
 }
